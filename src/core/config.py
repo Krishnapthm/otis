9bcc67a95a2bcb825317c4f24ba32ca18a98b366
Your Name <you@example.com>
2025-11-10 12:09:04 +0530

    simplified config

--- src/core/config.py
+++ src/core/config.py
@@ -1,7 +1,4 @@
-# src/core/config.py
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
-
 
 class Settings(BaseSettings):
     db_url: str
@@ -10,6 +7,5 @@ class Settings(BaseSettings):
         env_file=".env.fastapi",
         extra="ignore",
     )
-
 
 settings = Settings()
