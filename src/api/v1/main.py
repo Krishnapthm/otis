32093e43a69f17410b7bdbc40e344a67358a04ec
Your Name <you@example.com>
2025-11-10 12:17:27 +0530

    prefix update and new embeddings route

--- src/api/v1/main.py
+++ src/api/v1/main.py
@@ -1,12 +1,19 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.v1.routers import mcqs, docs, projects
+from src.api.v1.routers import embeddings, mcqs, docs, projects
 
 app = FastAPI(title="Otis")
 
-app.include_router(mcqs.router, prefix="/v1/mcq", tags=["MCQ"])
-app.include_router(docs.router, prefix="/v1/projects/documents", tags=["Documents"])
-app.include_router(projects.router, prefix="/v1/projects", tags=["Projects"])
+
+app.include_router(mcqs.router, prefix="/v1", tags=["MCQ"])
+app.include_router(docs.router, prefix="/v1", tags=["Documents"])
+app.include_router(projects.router, prefix="/v1", tags=["Projects"])
+app.include_router(embeddings.router, prefix="/v1", tags=["Embeddings"])
+
+
+# app.include_router(mcqs.router, prefix="/v1/mcq", tags=["MCQ"])
+# app.include_router(docs.router, prefix="/v1/projects/documents", tags=["Documents"])
+# app.include_router(projects.router, prefix="/v1/projects", tags=["Projects"])
 
 app.add_middleware(
     CORSMiddleware,
