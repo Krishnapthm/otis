86e65c78f2fa58bdc9860f459a071288336e4583
Your Name <you@example.com>
2025-12-06 07:38:58 +0530

    allow creds

--- src/api/v1/main.py
+++ src/api/v1/main.py
@@ -1,24 +1,23 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from src.api.v1.routers import embeddings, mcqs, docs, projects
+from src.api.v1.routers import auth, embeddings, mcqs, docs, projects
 
-app = FastAPI(title="Otis")
+app = FastAPI(title="Otis", swagger_ui_init_oauth={
+        "usePkceWithAuthorizationCodeGrant": True,
+    }
+)
 
 
 app.include_router(mcqs.router, prefix="/v1", tags=["MCQ"])
 app.include_router(docs.router, prefix="/v1", tags=["Documents"])
 app.include_router(projects.router, prefix="/v1", tags=["Projects"])
 app.include_router(embeddings.router, prefix="/v1", tags=["Embeddings"])
-
-
-# app.include_router(mcqs.router, prefix="/v1/mcq", tags=["MCQ"])
-# app.include_router(docs.router, prefix="/v1/projects/documents", tags=["Documents"])
-# app.include_router(projects.router, prefix="/v1/projects", tags=["Projects"])
+app.include_router(auth.router, prefix="/v1", tags=["Authentication"])
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=[True],
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
